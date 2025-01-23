3172bd3e17f171236ea9824183218c4797904ac3
julialuo02 <121737951+julialuo02@users.noreply.github.com>
2025-01-23 21:45:29 +0000

    finished home page

--- app/assets/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root to: { controller: "games", action: "home" }
-  get "/rock", to: { controller: "games", action: "rock" }
-  get "/paper", to: { controller: "games", action: "paper" }
-  get "/scissors", to: { controller: "games", action: "scissors" }
-end
---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,71 @@
+class GameController < ApplicationController
+  def rock
+    player_move = 0
+    @player_move_text = move_name(player_move)
+  
+    opponent_move = rand(3)
+    @opponent_move_text = move_name(opponent_move)
+  
+    if opponent_move == 1
+      @result = -1 # Loss
+    elsif opponent_move == 0
+      @result = 0 # Tie
+    else
+      @result = 1 # Win
+    end
+  
+    render({ template: "game_templates/games" })
+  end
+  
+
+  def paper
+    player_move = 1
+    @player_move_text = move_name(player_move)
+  
+    opponent_move = rand(3)
+    @opponent_move_text = move_name(opponent_move)
+  
+    if opponent_move == 2
+      @result = -1 # Loss
+    elsif opponent_move == 1
+      @result = 0 # Tie
+    else
+      @result = 1 # Win
+    end
+  
+    render({ template: "game_templates/games" })
+  end
+  
+
+  def scissors
+    player_move = 2
+    @player_move_text = move_name(player_move)
+  
+    opponent_move = rand(3)
+    @opponent_move_text = move_name(opponent_move)
+  
+    if opponent_move == 0
+      @result = -1 # Loss
+    elsif opponent_move == 2
+      @result = 0 # Tie
+    else
+      @result = 1 # Win
+    end
+  
+    render({ template: "game_templates/games" })
+  end
+  
+  def move_name(move)
+    if move == 0
+      "rock"
+    elsif move == 1
+      "paper"
+    else
+      "scissors"
+    end
+  end
+
+  def home
+    render template: "game_templates/home"
+  end
+end
